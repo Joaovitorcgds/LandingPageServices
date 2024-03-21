40e6b9484d6a317f846350a26d708777bd49b27c
joaov <joaovitorcgds@gmail.com>
2024-03-21 02:04:22 -0300

    fix accessibility v3

--- src/components/HeroArea/index.jsx
+++ src/components/HeroArea/index.jsx
@@ -10,7 +10,7 @@ export function HeroArea(){
             <span className="subtitleHeroArea">Criativas</span>
           </h1>
 
-          <p>Desenvolva sua presença digital e transforme suas ideias em soluções que irão impulsionar o sucesso de sua empresa.</p>
+          {/* <p>Desenvolva sua presença digital e transforme suas ideias em soluções que irão impulsionar o sucesso de sua empresa.</p> */}
 
           <a href="https://api.whatsapp.com/send?phone=5521975875130&text=Quero%20dar%20o%20primeiro%20passo%20para%20construir%20a%20identidade%20digital%20da%20minha%20empresa." className="btnHeroArea">Desenvolva Seu Futuro Digital</a>
         </div>
